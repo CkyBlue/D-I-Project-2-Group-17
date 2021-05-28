2d6b06d5b74bdca9a66a7d02a6f64a70223ad1b8
dolf321 <rami.r.richani02@hotmail.com>
2021-05-28 18:32:50 +0400

    Main ino file for part 1 created.

---
+++ Part1/Main.ino
@@ -0,0 +1,42 @@
+void setup()
+{
+    M5.begin(true, false, true);
+}
+
+uint8_t FSM = 0;
+
+void loop()
+{
+void loop()
+{
+    if (M5.Btn.wasPressed())
+    {
+
+        switch (FSM)
+        {
+        case 0:
+            setBuff(0x40, 0x00, 0x00);
+            break;
+        case 1:
+            setBuff(0x00, 0x40, 0x00);
+            break;
+        case 2:
+            setBuff(0x00, 0x00, 0x40);
+            break;
+        case 3:
+            setBuff(0x20, 0x20, 0x20);
+            break;
+        default:
+            break;
+        }
+        M5.dis.displaybuff(DisBuff);
+
+        FSM++;
+        if (FSM >= 4)
+        {
+            FSM = 0;
+        }
+    }
+
+}
+}
