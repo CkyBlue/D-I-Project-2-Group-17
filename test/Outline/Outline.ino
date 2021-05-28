f3630a11384e799fe99fd8e4b08bd702edf0eee2
Sakrit Karmacharya <34099661+CkyBlue@users.noreply.github.com>
2021-05-28 13:22:58 +0545

    Code Outline Proposition
    A proposition for dividing up tasks in the code

---
+++ test/Outline/Outline.ino
@@ -0,0 +1,48 @@
+#include "M5Atom.h"
+
+void setup()
+{
+    M5.begin(true, true, true);
+    delay(10);
+}
+
+uint8_t state = 0;
+// 0 - OFF, 1 - Manual Red Strobe, 2 - Manual White Strobe, 4 - Automatic Red Strobe, 5 - Automatic White Strobe
+
+bool isBraking(){}
+
+void strobe(CRGB color){}
+
+void loop()
+{
+    if (M5.Btn.wasPressed())
+    {
+        switch (state)
+        {
+        case 0: // OFF
+
+            break;
+        case 1: // Manual Red Strobe
+
+            break;
+        case 2: // Manual White Strobe
+
+            break;
+        case 3: // Automatic Red Strobe
+
+            break;
+        case 4: // Automatic White Strobe
+
+            break;
+        default:
+            break;
+        }
+
+
+        state++;
+        if (state >= 5){ state = 0; }
+    }
+
+    delay(50);
+    M5.update();
+}
