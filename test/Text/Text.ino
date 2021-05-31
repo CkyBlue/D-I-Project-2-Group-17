90035d725f9e42d57f2b6c0dd5c13e751beae34f
dolf321 <rami.r.richani02@hotmail.com>
2021-05-31 22:37:10 +0400

    did mistake and deleted text by accident REVERTED

--- test/Text/Text.ino
+++ test/Text/Text.ino
@@ -1,348 +1,65 @@
-#include <M5Atom.h>
+// Adafruit_NeoMatrix example for single NeoPixel Shield.
+// Scrolls 'Howdy' across the matrix in a portrait (vertical) orientation.
+
 #include <Adafruit_GFX.h>
 #include <Adafruit_NeoMatrix.h>
 #include <Adafruit_NeoPixel.h>
-#include <string>
-#include <iostream>
+
 #define PIN 27
-#define array_size 5
-#define LED_brightness 85
-using namespace std;
-float temp = 0;
-bool IMU6886Flag = true;
-bool isDownwards = false;
-bool positionChanged = false;
-bool wasPressed = false;
-uint8_t ctr = 0;
-uint8_t FSM = 0;
-uint8_t level = 0;
-char tilt = 'a';
-unsigned long millisOfLastTempUpdate = 0;
-unsigned long millisBetweenTempUpdate = 2000;
-unsigned long millisOfLastButtonUpdate = 0;
-unsigned long millisBetweenButtonUpdate = 50;
-unsigned long millisOfLastTiltUpdate = 0;
-unsigned long millisBetweenTiltUpdate = 400;
-unsigned long currentMillis = 0;
-class SensorData
-{
-private:
-    float acZ[array_size]; //aceleration Z for past 5 Timestamps
-    float acZbef;
-    int i;
-    float accX, accY, accZ;
-    float gyroX, gyroY, gyroZ;
-    float pitch, roll, yaw;
-    float Activetemperature;
 
-public:
-    SensorData()
-    {
-        accX = 0;
-        accY = 0;
-        accZ = 0;
-        gyroX = 0;
-        gyroY = 0;
-        gyroZ = 0;
-        Activetemperature = 0;
-        i = 0;
-    }
-    void update()
-    {
-        M5.IMU.getAhrsData(&pitch, &roll, &yaw);
-        M5.IMU.getAccelData(&accX, &accY, &accZ);
-    }
-    void fetchAcc()
-    {
-        M5.IMU.getAccelData(&accX, &accY, &accZ);
-        if (i >= array_size)
-        {
-            acZbef = acZ[array_size - 1];
-            i = 0;
-        }
-        else
-        {
-            acZ[i] = accZ;
-            i++;
-        }
-    }
-    void fetchAngles()
-    {
-        M5.IMU.getAhrsData(&pitch, &roll, &yaw);
-    }
-    float getActiveTemp()
-    {
-        M5.IMU.getTempData(&Activetemperature);
-        return Activetemperature;
-    }
-    bool isTap()
-    { //check if tapping
-        if (i == 0)
-        { //use before
-            if (abs(acZ[i] - acZbef) / abs(acZbef) > 0.03)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        else
-        {
-            if (abs(acZ[i] - acZ[i - 1]) / abs(acZ[i - 1]) > 0.03)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-    }
-    bool isUp() //checks if vertically upwards
-    {
-        if ((abs(roll)) < 1 && (abs(pitch)) < 1 && (abs(yaw) < 1))
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
-    char isTilted() // returns r if tilted right, l if tilted left, u if held upwards, d if held downwards
-    {
-        if (abs(accX) < 0.6 && abs(accY) < 0.6 && accZ < 0) // device held upwards
-            return 'u';
-        else if (accX > 0.4)
-            return 'r';
-        else if (accX < -0.4)
-            return 'l';
-        else // device held downwards
-            return 'd';
-    }
-    void levelChangerSensor(uint8_t &level, bool &positionChanged, bool &isDownwards)
-    {
-        char orientation = isTilted();
-        if (orientation == 'r')
-        {
-            if (level == 5)
-                level = 1;
-            else
-                level++;
-            positionChanged = true;
-        }
-        else if (orientation == 'l')
-        {
-            if (level == 0 || level == 1)
-                level = 5;
-            else
-                level--;
-            positionChanged = true;
-        }
-        else if (orientation == 'd')
-        {
-            level = 0;
-            Serial.printf("tilted downwards\n");
-            isDownwards = true;
-            positionChanged = true;
-        }
-    }
-};
-//scrolling text inspired by https://github.com/lukasmaximus89/M5Atom-Resources/blob/master/AtomScrollingText/AtomScrollingText.ino
-class display
-{
-private:
-    Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(5, 5, PIN,
-                                                   NEO_MATRIX_TOP + NEO_MATRIX_RIGHT +
-                                                       NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
-                                                   NEO_GRB + NEO_KHZ800);
-    uint16_t color;
-    float temperature;
-    char unit;int x,pass;
-public:
-    display()
-    {
-        //farenheight C and kelvin
-        matrix.begin();
-        matrix.setTextWrap(false);
-        matrix.setBrightness(LED_brightness);
-        matrix.setTextColor(color);
-        color = matrix.Color(255, 255, 255);
-        x=0;
-        pass=0;
-        unit = 'C';
-    }
-    void changeUnit()
-    {
-        if(unit=='C'){
-            temperature+=273.15;
-            unit=='K';
-        } else if(unit=='K') {
-            temperature=(temperature-273.15)*9/5+32;
-            unit=='F';
-        } else {
-            temperature=(temperature-32)*5/9;
-            unit=='C';
-        }
-    }
-    void displayTemperature()
-    {
-        matrix.fillScreen(0);
-        matrix.setCursor(x, 0);
-        String u="";
-        u.concat(temperature);
-        u+=unit;
-        matrix.print(x);
-        if (--x < -36)
-        {
-            x = matrix.width();
-            if (++pass >= 3)
-                pass = 0;
-            matrix.setTextColor(color);
-        }
-        matrix.show();
-    }
-    
-};
+// MATRIX DECLARATION:
+// Parameter 1 = width of NeoPixel matrix
+// Parameter 2 = height of matrix
+// Parameter 3 = pin number (most are valid)
+// Parameter 4 = matrix layout flags, add together as needed:
+//   NEO_MATRIX_TOP, NEO_MATRIX_BOTTOM, NEO_MATRIX_LEFT, NEO_MATRIX_RIGHT:
+//     Position of the FIRST LED in the matrix; pick two, e.g.
+//     NEO_MATRIX_TOP + NEO_MATRIX_LEFT for the top-left corner.
+//   NEO_MATRIX_ROWS, NEO_MATRIX_COLUMNS: LEDs are arranged in horizontal
+//     rows or in vertical columns, respectively; pick one or the other.
+//   NEO_MATRIX_PROGRESSIVE, NEO_MATRIX_ZIGZAG: all rows/columns proceed
+//     in the same order, or alternate lines reverse direction; pick one.
+//   See example below for these values in action.
+// Parameter 5 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_GRBW    Pixels are wired for GRBW bitstream (RGB+W NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
 
-SensorData tl;
-void getUnit()
-{
+
+// Example for NeoPixel Shield.  In this application we'd like to use it
+// as a 5x8 tall matrix, with the USB port positioned at the top of the
+// Arduino.  When held that way, the first pixel is at the top right, and
+// lines are arranged in columns, progressive order.  The shield uses
+// 800 KHz (v2) pixels that expect GRB color data.
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(5, 5, PIN,
+  NEO_MATRIX_TOP     + NEO_MATRIX_RIGHT +
+  NEO_MATRIX_COLUMNS + NEO_MATRIX_PROGRESSIVE,
+  NEO_GRB            + NEO_KHZ800);
+
+const uint16_t colors[] = {
+  matrix.Color(255, 0, 0), matrix.Color(0, 255, 0), matrix.Color(0, 0, 255) };
+
+void setup() {
+  matrix.begin();
+  matrix.setTextWrap(false);
+  matrix.setBrightness(40);
+  matrix.setTextColor(colors[0]);
 }
 
-void mode4()
-{
-}
+int x    = matrix.width();
+int pass = 0;
 
-void setup()
-{
-    M5.begin(true, false, true);
-    if (M5.IMU.Init() != 0)
-        IMU6886Flag = false;
-    else
-        IMU6886Flag = true;
-}
-void loop()
-{
-    if (IMU6886Flag == true)
-    {
-        tl.update();
-        tilt = tl.isTilted();
-        tl.fetchAcc();
-        if ((M5.Btn.wasPressed() || tl.isTap()) && (tilt == 'u'))
-        {
-            millisOfLastTiltUpdate = millis();
-            Serial.printf("upwards\n");
-            isDownwards = false;
-            while (isDownwards == false)
-            {
-                switch (level)
-                {
-                case 0:
-                    Serial.printf("screen activated\n");
-                    positionChanged = false;
-                    while (positionChanged == false)
-                    {
-                        currentMillis = millis();
-                        if (currentMillis - millisOfLastTiltUpdate > millisBetweenTiltUpdate)
-                        {
-                            tilt = tl.isTilted();
-                            millisOfLastTiltUpdate = millis();
-                            tl.levelChangerSensor(level, positionChanged, isDownwards);
-                        }
-                    }
-                    break;
-                case 1:
-                    Serial.printf("mode 1\n");
-                    positionChanged = false;
-                    while (positionChanged == false)
-                    {
-                        currentMillis = millis();
-                        if (currentMillis - millisOfLastTiltUpdate > millisBetweenTiltUpdate)
-                        {
-                            tilt = tl.isTilted();
-                            millisOfLastTiltUpdate = millis();
-                            tl.levelChangerSensor(level, positionChanged, isDownwards);
-                        }
-                    }
-                    break;
-                case 2:
-                    Serial.printf("mode 2\n");
-                    positionChanged = false;
-                    while (positionChanged == false)
-                    {
-                        currentMillis = millis();
-                        if (currentMillis - millisOfLastTiltUpdate > millisBetweenTiltUpdate)
-                        {
-                            tilt = tl.isTilted();
-                            millisOfLastTiltUpdate = millis();
-                            tl.levelChangerSensor(level, positionChanged, isDownwards);
-                        }
-                    }
-                    break;
-                case 3:
-                    Serial.printf("mode 3\n");
-                    positionChanged = false;
-                    while (positionChanged == false)
-                    {
-                        currentMillis = millis();
-                        if (currentMillis - millisOfLastTiltUpdate > millisBetweenTiltUpdate)
-                        {
-                            tilt = tl.isTilted();
-                            millisOfLastTiltUpdate = millis();
-                            tl.levelChangerSensor(level, positionChanged, isDownwards);
-                        }
-                    }
-                    break;
-                case 4:
-                    Serial.printf("mode 4\n");
-                    mode4();
-                    positionChanged = false;
-                    while (positionChanged == false)
-                    {
-                        currentMillis = millis();
-                        if (currentMillis - millisOfLastTiltUpdate > millisBetweenTiltUpdate)
-                        {
-                            tilt = tl.isTilted();
-                            millisOfLastTiltUpdate = millis();
-                            tl.levelChangerSensor(level, positionChanged, isDownwards);
-                        }
-                    }
-                    break;
-                case 5:
-                    Serial.printf("mode 5\n");
-                    positionChanged = false;
-                    while (positionChanged == false)
-                    {
-                        currentMillis = millis();
-                        if (currentMillis - millisOfLastTiltUpdate > millisBetweenTiltUpdate)
-                        {
-                            tilt = tl.isTilted();
-                            millisOfLastTiltUpdate = millis();
-                            tl.levelChangerSensor(level, positionChanged, isDownwards);
-                        }
-                    }
-                    break;
-                default:
-                    break;
-                }
-            }
-        }
-    }
-    currentMillis = millis();
-
-    if (currentMillis - millisOfLastButtonUpdate > millisBetweenButtonUpdate)
-    {
-        M5.update();
-        millisOfLastButtonUpdate = millis();
-    }
-
-    if (currentMillis - millisOfLastTempUpdate > millisBetweenTempUpdate)
-    {
-        M5.IMU.getTempData(&temp);
-        Serial.printf("Temperature: %.2f °C \n", temp);
-        millisOfLastTempUpdate = millis();
-    }
+void loop() {
+  matrix.fillScreen(0);
+  matrix.setCursor(x, 0);
+  matrix.print(F("HELLO WORLD "));
+  if(--x < -96) {
+    x = matrix.width();
+    if(++pass >= 3) pass = 0;
+    matrix.setTextColor(colors[pass]);
+  }
+  matrix.show();
+  delay(80);
 }
